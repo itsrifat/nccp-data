dc16b6549f1134e112ba7319b79ef87c60f44b86
Moinul Hossain Rifat <moinul.cse@gmail.com>
2014-09-26 15:28:07 -0700

    added basic structure for this module
    This is a java module that will be used to generate xml metadata files
    from nccp's raw metadata files(generated by the scripts) according to
    ISO 19115. It uses apache sis which is an implementation of ISO 19115
    spec

---
+++ iso19115-xml-metadata-generator/src/main/java/edu/unr/nccp/App.java
@@ -0,0 +1,13 @@
+package edu.unr.nccp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
